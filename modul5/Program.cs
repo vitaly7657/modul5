30c8300481a6f4cdc2db41f98b91fa2904b0a963
Saske <Saske@DESKTOP-1QV8TE3>
2022-07-17 20:15:33 +0300

    Добавьте файлы проекта.

---
+++ modul5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace modul5
+{
+    internal class Program
+    {
+        static string[] SplitText(string Text)
+        {
+            string[] TextToMass = Text.Split(' ');
+            return TextToMass;
+        }
+        static void OutputText(string[] LineText)
+        {
+            for(int i = 0; i < LineText.Length; i++)
+            {
+                Console.WriteLine($"{i+1,5} слово: {LineText[i]}");
+            }                        
+        }
+
+        static void ReversWords(string inputPhrase)
+        {
+            string[] ReverseMass = SplitText(inputPhrase);    
+            Console.Write("Предложение в обратном порядке: ");
+            for (int i = ReverseMass.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{ReverseMass[i]} ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            //задание 1
+            Console.WriteLine("задание 1");
+            Console.Write("Введите предложение: ");
+            string TextToSplit = Console.ReadLine();
+            string[] OutputMass = SplitText(TextToSplit);
+            OutputText(OutputMass);
+            Console.ReadKey();
+
+            //задание 2
+            Console.WriteLine("\nзадание 2");
+            Console.Write("Введите предложение: ");
+            string TextToReverse = Console.ReadLine();
+            ReversWords(TextToReverse);
+            Console.ReadKey();
+        }
+    }
+}
